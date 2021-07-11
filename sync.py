5588016e537c32cc2b616df77615558f1013acc5
Vladislav Tkachenko <webkadiz@gmail.com>
2021-07-11 15:52:01 +0300

    add interactive option to sync

--- sync.py
+++ sync.py
@@ -7,28 +7,34 @@ import sys
 
 def sync(src, dest):
     files = open("skel")
+    cpFlags = "-a"
+
+    if args.interactive:
+        cpFlags += " -i"
 
     for file in files:
         file = file.strip()
         fullSrc = src + file
         fullDest = dest + file
-        dirname = os.path.dirname(fullDest) 
+        dirname = os.path.dirname(fullDest)
 
         os.system(f"mkdir -p '{dirname}'")
 
         if os.path.isdir(fullDest):
-            command = f"cp -a '{fullSrc}' '{dirname}'"
-            print(command)
-            os.system(command)
+            command = f"cp {cpFlags} '{fullSrc}' '{dirname}'"
         else: 
-            command = f"cp -a '{fullSrc}' '{dirname}'"
+            command = f"cp {cpFlags} '{fullSrc}' '{dirname}'"
+
+        if not args.interactive:
             print(command)
-            os.system(command)
+
+        os.system(command)
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--from-skel", action="store_true", help="sync from skel to system")
 parser.add_argument("-t", "--to-skel", action="store_true", help="sync from system to skel")
+parser.add_argument("-i", "--interactive", action="store_true", help="prompt before overwrite")
 args = parser.parse_args(sys.argv[1:])
 
 if args.from_skel:
