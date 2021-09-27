23698194343156338fe00c2380388ed6a9e57b7c
Vladislav Tkachenko <webkadiz@gmail.com>
2021-09-27 20:15:47 +0000

    new startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+# parse arguments
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -p|--password)
+      PASSWORD="$2"
+      shift
+      shift
+      ;;
+    *)
+      POSITIONAL+=("$1")
+      shift
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [[ ! $PASSWORD ]]; then
+    echo "set password through -p option"
+    exit
+fi
+
+# change shell to zsh
+chsh < <(echo -e "$PASSWORD\n/usr/bin/zsh")
+
+# sync configuration
+./sync.py -f
+
+# install primary packages
+sudo pacman -S $(cat packages/archlinux/pacman-primary)
+
 # install packer
 git clone https://github.com/wbthomason/packer.nvim \
     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
@@ -11,8 +45,16 @@ mkdir -p ~/.local/share/nvim/backup
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
 
 # install nvm
+# TODO find last nvm version from github
+mkdir ~/.config/nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
-# sync configuration
-./sync.py -f
+cd /tmp
+pushd .
+git clone https://aur.archlinux.org/yay.git
+cd yay
+pushd .
+makepkg -si
+popd
+popd
 
